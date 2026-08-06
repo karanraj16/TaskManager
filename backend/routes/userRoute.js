cc3fff47fadc60345727ce0c3bd8be4498b6703f
karanraj16 <karanraj.sankar16@gmail.com>
2026-08-06 15:08:36 +0000

    Convert all backend files to ES modules

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  protect  } from "../middleware/authMiddleware.js";
+import  protect   from "../middleware/authMiddleware.js";
 import User from "../models/User.js";
 
 const router = express.Router();
